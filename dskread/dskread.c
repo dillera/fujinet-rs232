b30e34a2830f0d91f3a214a082f8785e163adbe2
Chris Osborn <fozztexx@fozztexx.com>
2025-01-28 10:26:35 -0800

    Convert dskread to Open Watcom

--- dskread/dskread.c
+++ dskread/dskread.c
@@ -4,11 +4,11 @@
 #include "fujicom.h"
 #include "com.h"
 
-cmdFrame_t *cmd;
+cmdFrame_t cmd;
 
 unsigned char buf[512];
 
-int main(void)
+int main(int argc, char *argv[])
 {
     unsigned int sector = atoi(argv[1]);
     unsigned int i=0;
@@ -19,12 +19,13 @@ int main(void)
         exit(1);
     }
 
-    cmd.ddev = 0x31;  /* Drive 1 */
-    cmd.dcomnd = 'R'; /* Read */
-    cmd.daux1 = sector & 0xFF;
-    cmd.daux2 = sector >> 8;
+    cmd.device = DEVICEID_DISK;  /* Drive 1 */
+    cmd.comnd = 'R'; /* Read */
+    cmd.aux1 = sector & 0xFF;
+    cmd.aux2 = sector >> 8;
 
-    printf("fujicom_command_read(%c) for sector %u",fujicom_command_read(&cmd, buf, sizeof(buf)),sector);
+    printf("fujicom_command_read(%c) for sector %u",
+	   fujicom_command_read(&cmd, buf, sizeof(buf)), sector);
 
     for (i=0;i<sizeof(buf);i++)
     {
