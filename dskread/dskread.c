bcd04aa3f640d13454f61b789e3db9b67cff2acd
Chris Osborn <fozztexx@fozztexx.com>
2025-01-28 10:42:14 -0800

    Need to call fujicom_init()

--- dskread/dskread.c
+++ dskread/dskread.c
@@ -10,27 +10,27 @@ unsigned char buf[512];
 
 int main(int argc, char *argv[])
 {
-    unsigned int sector = atoi(argv[1]);
-    unsigned int i=0;
+  unsigned int sector = atoi(argv[1]);
+  unsigned int i=0;
     
-    if (argc<2)
-    {
-        printf("DSKREAD <0-sector number>\n");
-        exit(1);
-    }
+  if (argc<2) {
+    printf("DSKREAD <0-sector number>\n");
+    exit(1);
+  }
 
-    cmd.device = DEVICEID_DISK;  /* Drive 1 */
-    cmd.comnd = 'R'; /* Read */
-    cmd.aux1 = sector & 0xFF;
-    cmd.aux2 = sector >> 8;
+  cmd.device = DEVICEID_DISK;  /* Drive 1 */
+  cmd.comnd = 'R'; /* Read */
+  cmd.aux1 = sector & 0xFF;
+  cmd.aux2 = sector >> 8;
 
-    printf("fujicom_command_read(%i) for sector %u\n",
-	   fujicom_command_read(&cmd, buf, sizeof(buf)), sector);
+  fujicom_init();
+  
+  printf("fujicom_command_read(%i) for sector %u\n",
+	 fujicom_command_read(&cmd, buf, sizeof(buf)), sector);
 
-    for (i=0;i<sizeof(buf);i++)
-    {
-        printf("%02x ",buf[i]);
-    }
+  for (i=0;i<sizeof(buf);i++) {
+    printf("%02x ",buf[i]);
+  }
 
-    return 0;
+  return 0;
 }
