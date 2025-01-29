a983413eaa8f12152af21532d35e00c72038e909
Chris Osborn <fozztexx@fozztexx.com>
2025-01-29 15:03:59 -0800

    Dump hex with offsets and ASCII rep

--- dskread/dskread.c
+++ dskread/dskread.c
@@ -8,6 +8,8 @@ cmdFrame_t cmd;
 
 unsigned char buf[512];
 
+void dumpHex(uint8_t far *buffer, uint16_t count);
+
 int main(int argc, char *argv[])
 {
   unsigned int sector = atoi(argv[1]);
@@ -28,9 +30,37 @@ int main(int argc, char *argv[])
   printf("fujicom_command_read(%i) for sector %u\n",
 	 fujicom_command_read(&cmd, buf, sizeof(buf)), sector);
 
-  for (i = 0; i < sizeof(buf); i++) {
-    printf("%02x ",buf[i]);
-  }
+  dumpHex(buf, sizeof(buf));
 
   return 0;
+}
+
+void dumpHex(uint8_t far *buffer, uint16_t count)
+{
+  int outer, inner;
+  uint8_t c, is_err;
+
+
+  for (outer = 0; outer < count; outer += 16) {
+    printf("%08x  ", outer);
+    for (inner = 0; inner < 16; inner++) {
+      if (inner + outer < count) {
+        c = buffer[inner + outer];
+	printf("%02x ", c);
+      }
+      else
+	printf("   ");
+    }
+    printf(" |");
+    for (inner = 0; inner < 16 && inner + outer < count; inner++) {
+      c = buffer[inner + outer];
+      if (c >= ' ' && c <= 0x7f)
+	putchar(c);
+      else
+	putchar('.');
+    }
+    printf("|\r\n");
+  }
+
+  return;
 }
---
+++ sys/fujinet.h
@@ -0,0 +1,6 @@
+#ifndef _FUJINET_H
+#define _FUJINET_H
+
+#define MAX_FN_DEV	8
+
+#endif /* _FUJINET_H */
